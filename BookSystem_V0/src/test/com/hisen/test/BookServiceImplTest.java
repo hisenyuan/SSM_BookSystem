506f92b2d8e665bb3c150f0e391aa6e79f13d80d
hisenyuan <2811923@qq.com>
2017-05-03 20:22:49 +0800

    新增的测试类

---
+++ BookSystem_V0/src/test/com/hisen/test/BookServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.hisen.test;
+
+import com.hisen.entity.Book;
+import com.hisen.service.BookService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by hisenyuan on 2017/5/3 at 20:16.
+ */
+public class BookServiceImplTest extends BookDaoTest {
+  @Autowired
+  private BookService bookService;
+
+  @Test
+  public void getById() {
+    //在service里面改了一下名字，其实就是dao里面的queryById
+    Book book = bookService.getById(101);
+    System.out.println(book);
+  }
+}
