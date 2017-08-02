c23e43d0181171431f581f26578cbdd5c96df684
hisenyuan <2811923@qq.com>
2017-08-02 12:34:30 +0800

    图书管理系统V2版本： 测试类

---
+++ BookSystem_V2/src/test/AppointmentServiceImplTest.java
@@ -0,0 +1,26 @@
+import com.hisen.dao.form.AppointmentForm;
+import com.hisen.service.AppointmentService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by hisenyuan on 2017/8/2 at 11:03.
+ */
+public class AppointmentServiceImplTest extends BaseTest {
+
+  @Autowired
+  private AppointmentService appointmentService;
+
+  /**
+   * 测试预约功能
+   */
+  @Test
+  public void insertApponit() {
+    AppointmentForm form = new AppointmentForm();
+    form.setHoldDay(String.valueOf(10));
+    form.setBookId(103);
+    form.setUserNumber(20080808);
+    int appoint = appointmentService.appoint(form);
+    System.out.println(appoint);
+  }
+}
