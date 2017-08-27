157428e1858baaccaa4b99f998a7043ed8b65057
hisenyuan <hisenyuan@gmail.com>
2017-08-27 17:26:03 +0800

    图书管理系统V3版本：JWT认证基础完成 - 更新建表语句

---
+++ BookSystem_V3/src/main/java/com/hisen/dao/sql/booksystem_user_info.sql
@@ -0,0 +1,1 @@
+INSERT INTO booksystem.user_info (name, gender, age, pwd, user_state, create_time, last_time, update_time, user_type) VALUES ('hisen', 1, 25, '28-80-51-21-1357-57-120-4292-821873-76-6107', 0, '2017-08-27', '2017-08-27', '2017-08-27', 0);
