bbd54dbef689adde964c8ec03b3e59efe336061e
hisenyuan <hisenyuan@gmail.com>
2017-08-27 16:56:26 +0800

    图书管理系统V3版本：通用请求和返回

---
+++ BookSystem_V3/src/main/java/com/hisen/bean/request/CommonRequest.java
@@ -0,0 +1,15 @@
+package com.hisen.bean.request;
+
+import java.util.Date;
+
+/**
+ * Created by hisen on 17-8-26.
+ * E-mail: hisenyuan@gmail.com
+ */
+public class CommonRequest {
+  private Date time = new Date();
+
+  public Date getTime() {
+    return time;
+  }
+}
---
+++ BookSystem_V3/src/main/java/com/hisen/bean/response/CommonResponse.java
@@ -0,0 +1,27 @@
+package com.hisen.bean.response;
+
+/**
+ * Created by hisen on 17-8-26.
+ * E-mail: hisenyuan@gmail.com
+ */
+public class CommonResponse {
+
+  private int resCode;
+  private String resMsg;
+
+  public int getResCode() {
+    return resCode;
+  }
+
+  public void setResCode(int resCode) {
+    this.resCode = resCode;
+  }
+
+  public String getResMsg() {
+    return resMsg;
+  }
+
+  public void setResMsg(String resMsg) {
+    this.resMsg = resMsg;
+  }
+}
